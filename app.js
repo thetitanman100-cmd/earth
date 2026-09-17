fcf09ebf981dca8b8a402407a92255938c229cd8
thetitanman100-cmd <thetitanman100@gmail.com>
2026-09-17 19:05:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.181.2/build/three.module.js";
-import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.181.2/examples/jsm/controls/OrbitControls.js";
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const $ = (selector) => document.querySelector(selector);
 const globeHost = $("#globe");
